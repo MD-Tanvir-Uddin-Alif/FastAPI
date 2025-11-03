4a6a9389ff522c90e6f583a19327c544c49fee30
MD.Tanvir Uddin Alif <alif252669@gmail.com>
2025-11-03 11:24:29 +0600

    read_single, and list done

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 from sqlalchemy.orm import Session
 from fastapi import FastAPI, Depends
 from database import get_db, Base, engine
-from schema import ProductCreate
+from schema import ProductCreate, BaseModel
 from models import Product
 
 
@@ -17,3 +17,15 @@ def product_create(product: ProductCreate, db: Session = Depends(get_db)):
     db.commit()
     db.refresh(new_Product)
     return new_Product
+
+
+@app.get('/all/products')
+def get_all_product(db: Session = Depends(get_db)):
+    all_products = db.query(Product).all()
+    return all_products
+
+
+@app.get('/product/{p_id}')
+def Single_Product(p_id: int, db: Session = Depends(get_db)):
+    product = db.query(Product).filter(Product.id==p_id).first()
+    return product
--- schema.py
+++ schema.py
@@ -2,6 +2,11 @@ from pydantic import BaseModel
 
 
 
+class BaseProduct(BaseModel):
+    name: str
+    description : str
+
+
 class ProductCreate(BaseModel):
     name: str
     description: str
